e10dbd810f791a9af479c634399323c2e368824c
배석현 <bjh4966@naver.com>
2024-09-02 21:24:49 +0900

    13241번 실버

---
+++ src/silver/LCM1.java
@@ -0,0 +1,40 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class LCM1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+
+        long result = lcm(A, B);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long lcm(long a, long b) {
+        return a * (b / gcd(a, b));
+    }
+}
