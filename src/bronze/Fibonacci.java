5058c77eb4f1767d2871386fadb9e1b306fbf4c5
배석현 <bjh4966@naver.com>
2024-09-14 20:04:48 +0900

    10870번 브론즈

---
+++ src/bronze/Fibonacci.java
@@ -0,0 +1,27 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Fibonacci {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        // N이 0인 경우는 바로 처리
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        System.out.println(arr[N]);
+    }
+}
