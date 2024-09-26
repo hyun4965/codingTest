6a2d44776732d85a40eaf4526d13d20dbf8d77b8
배석현 <bjh4966@naver.com>
2024-09-26 17:09:46 +0900

    15651번 실버

--- src/silver/NandM3.java
+++ src/silver/NandM3.java
@@ -1,6 +1,35 @@
 package silver;
 
-
+//import java.util.Scanner;
+//
+//public class NandM3 {
+//    static int n,m;
+////    static boolean[] isvisit;
+//    static int[] seq;
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        m = sc.nextInt();
+//        seq = new int[n+1];
+//        isvisit = new boolean[n+1];
+//        def(0,1);
+//    }
+//    public static void def(int depth, int start) {
+//        if (depth == m) {
+//            for (int i = 0; i < m; i++) {
+//                System.out.print(seq[i] + " ");
+//            }
+//            System.out.println();
+//            return;
+//        }
+//
+//        for (int i = 1; i <= n; i++) {
+//            seq[depth] = i;
+//            def(depth + 1, i);
+//        }
+//
+//    }
+//}
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
