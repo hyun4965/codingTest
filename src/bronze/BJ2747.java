ad213fb02e150b8d0cc88cb57f2ff5f025ef348e
배석현 <bjh4966@naver.com>
2025-02-13 15:51:13 +0900

    2747 DP

---
+++ src/bronze/BJ2747.java
@@ -0,0 +1,28 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class BJ2747 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[n];
+    }
+}
