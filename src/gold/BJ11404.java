91509904bc63f3639244226bdad397306ce2d38b
배석현 <bjh4966@naver.com>
2025-03-31 18:58:39 +0900

    11404번 플로이드워셜

---
+++ src/gold/BJ11404.java
@@ -0,0 +1,56 @@
+package gold;
+
+import java.util.Scanner;
+
+public class BJ11404 {
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 도시의 개수
+        int m = sc.nextInt(); // 버스의 개수
+
+        int[][] dist = new int[n + 1][n + 1];
+
+        // 초기화
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = INF;
+                }
+            }
+        }
+
+        // 버스 정보 입력
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
