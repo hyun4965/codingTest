8d49186acf076d4180d3997570db3a4420b10b30
배석현 <bjh4966@naver.com>
2025-04-15 08:14:38 +0900

    1759 DFS

--- src/gold/BJ1759.java
+++ src/gold/BJ1759.java
@@ -37,16 +37,21 @@ public class BJ1759 {
         }
     }
 
-    static boolean isValid(char[] password) {
-        int vowel = 0;
-        int consonant = 0;
-        for (char c : password) {
-            if ("aeiou".indexOf(c) >= 0) {
-                vowel++;
+    // 자음이 2개이상 모음이 1개 이상인지 판별하는 로직
+    public static boolean isValid(char[] code) {
+        int c = 0; // 자음의 수
+        int v = 0; // 모음의 수
+        for (int i = 0; i < code.length; i++) {
+            if (code[i] == 'a' || code[i] == 'e' || code[i] == 'i' || code[i] == 'o' || code[i] == 'u') {
+                v++; // 모음일 경우 v++
             } else {
-                consonant++;
+                c++; // 자음일 경우 c++
             }
         }
-        return vowel >= 1 && consonant >= 2;
+        if (c >= 2 && v >= 1) {
+            return true; // 자음 2개 이상, 모음 1개이상 일 경우 true 반환
+        } else {
+            return false; // 아닐경우 false 반환
+        }
     }
 }
