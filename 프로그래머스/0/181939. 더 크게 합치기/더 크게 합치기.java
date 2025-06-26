af4517d9fbad90b253720996ef9704f2d76d202d
BAE <50188017+hyun4965@users.noreply.github.com>
2025-06-26 16:47:57 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.79 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + b);
+        int ba = Integer.parseInt(String.valueOf(b) + a);
+        
+        return Math.max(ab, ba);
+    }
+}
