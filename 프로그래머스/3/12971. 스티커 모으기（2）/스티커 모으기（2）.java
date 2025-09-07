69f5e5d246e343e66203206507934e9713836311
BAE <50188017+hyun4965@users.noreply.github.com>
2025-09-07 16:29:50 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.16 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,28 +1,26 @@
 class Solution {
     public int solution(int[] sticker) {
+        if (sticker.length == 1) {
+            return sticker[0];
+        }
+
         int n = sticker.length;
-        if (n == 1) return sticker[0]; 
 
-        int prev2 = 0, prev1 = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int take = prev2 + sticker[i]; 
-            int skip = prev1;
-            int cur  = Math.max(take, skip);
-            prev2 = prev1;
-            prev1 = cur;
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+        for (int i = 2; i < n - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
         }
-        int case1 = prev1;
 
-        prev2 = 0; prev1 = 0;
-        for (int i = 1; i < n; i++) {
-            int take = prev2 + sticker[i];
-            int skip = prev1;
-            int cur  = Math.max(take, skip);
-            prev2 = prev1;
-            prev1 = cur;
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
         }
-        int case2 = prev1;
 
-        return Math.max(case1, case2);
+        return Math.max(dp1[n - 2], dp2[n - 1]);
     }
 }
