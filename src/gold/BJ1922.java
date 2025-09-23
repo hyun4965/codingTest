f4ff25b8387e0849e0e20fcb2150033c7372b4f8
배석현 <bjh4966@naver.com>
2025-09-23 16:42:52 +0900

    1922 Kruskal

---
+++ src/gold/BJ1922.java
@@ -0,0 +1,76 @@
+package gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BJ1922 {
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static int find(int[] parent, int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent, parent[x]);
+    }
+
+    public static void union(int[] parent, int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(a, b, c));
+        }
+
+        Collections.sort(edges);
+
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int totalCost = 0;
+        for (Edge edge : edges) {
+            if (find(parent, edge.from) != find(parent, edge.to)) {
+                union(parent, edge.from, edge.to);
+                totalCost += edge.cost;
+            }
+        }
+
+        System.out.println(totalCost);
+    }
+}
