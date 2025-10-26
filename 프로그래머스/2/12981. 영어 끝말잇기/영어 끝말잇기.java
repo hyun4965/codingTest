725420749be090cb19abc9c6b83ffe6f6a680e28
BAE <50188017+hyun4965@users.noreply.github.com>
2025-10-26 13:08:45 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,29 +1,32 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        List<String> list = new ArrayList<String>();
-        boolean flag = true;
+        Set<String> usedWords = new HashSet<>();
+
+        usedWords.add(words[0]);
         
-        for(int i=0; i<words.length; i++){
-            if(list.contains(words[i])){  
-                answer[0] = (i%n) + 1;
-                answer[1] = (i/n) + 1;
-                flag = false;
-                break;
+        for (int i = 1; i < words.length; i++) {
+            String currentWord = words[i];
+            String prevWord = words[i - 1];
+            
+            char lastChar = prevWord.charAt(prevWord.length() - 1);
+            char firstChar = currentWord.charAt(0);
+            
+            if (lastChar != firstChar || usedWords.contains(currentWord)) {
+
+                int personNumber = (i % n) + 1;
+
+                int turnNumber = (i / n) + 1;
+                
+                answer[0] = personNumber;
+                answer[1] = turnNumber;
+                return answer;
             }
             
-            list.add(words[i]); 
-            
-            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
-                answer[0] = (i%n) + 1;
-                answer[1] = (i/n) + 1;
-                flag = false;
-                break;
-            }
+            usedWords.add(currentWord);
         }
-        if(flag) return new int[]{0, 0};
+
         return answer;
     }
 }
