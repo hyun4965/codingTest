750ce3b080c6c00b6330b778cf79662428b8c8bb
BAE <50188017+hyun4965@users.noreply.github.com>
2025-10-31 14:47:33 +0900

    [level 3] Title: 여행경로, Time: 0.63 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -4,26 +4,48 @@ class Solution {
 
     static List<String> list = new ArrayList<>();
     static boolean[] visited;
+    static boolean isSuccess = false; 
 
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
-        dfs(0, "ICN", "ICN", tickets);
-        Collections.sort(list);
-        return list.get(0).split(" ");
+        
+        visited = new boolean[tickets.length]; 
+        
+        Arrays.sort(tickets, (a, b) -> {
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
+            }
+            return a[0].compareTo(b[0]);
+        });
+        
+        dfs("ICN", tickets);
+        
+        return list.toArray(new String[0]);
     }
 
-    static void dfs(int depth, String now, String path, String[][] tickets){
-        if (depth == tickets.length) {
-            list.add(path);
+    static void dfs(String now, String[][] tickets) {
+        list.add(now);
+        
+        if (list.size() == tickets.length + 1) {
+            isSuccess = true;
             return;
         }
-
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i] && now.equals(tickets[i][0])) {
-                visited[i] = true;
-                dfs(depth+1, tickets[i][1], path + " " +tickets[i][1], tickets);
-                visited[i] = false;
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(now)) {
+                
+                visited[i] = true; 
+                dfs(tickets[i][1], tickets); 
+                
+                if (isSuccess) {
+                    return;
+                }
+                
+                visited[i] = false; 
             }
+        }
+
+        if (!isSuccess) {
+            list.remove(list.size() - 1);
         }
     }
 }
