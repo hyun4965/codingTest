50e65603a7bc450f314e62f719f7569f9881ffc0
BAE <50188017+hyun4965@users.noreply.github.com>
2025-10-31 21:42:27 +0900

    [level 3] Title: 야근 지수, Time: 116.89 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0; i < works.length; i++) {
+            queue.add(works[i]);
+        }
+        
+        while(n > 0) {
+            int work = queue.poll();
+            if(work == 0) {
+                break; 
+            }
+            work -= 1; 
+            queue.offer(work);
+            n -= 1;
+        }
+        
+        long answer = 0;
+        for(int work : queue) {
+            answer += work * work;
+        }
+        return answer;
+    }
+}
