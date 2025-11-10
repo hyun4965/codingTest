532558837b47c8bf14b203442e59a52567cfc98c
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-10 12:12:16 +0900

    [D1] Title: 신문 헤드라인, Time: 104 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+        System.out.print(s.toUpperCase());
+        
+	}
+}
