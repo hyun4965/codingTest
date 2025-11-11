32193ad15ce81cb5c1979314b40995ba1c247c4c
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-11 10:15:53 +0900

    [D3] Title: 장애물 경주 난이도, Time: 144 ms, Memory: 35,648 KB -BaekjoonHub

---
+++ SWEA/D3/6730. 장애물 경주 난이도/장애물 경주 난이도.java
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int upMax = 0;  
+            int downMax = 0;
+
+            for (int i = 1; i < N; i++) {
+                int diff = arr[i] - arr[i - 1];
+
+                if (diff > 0) {     
+                    upMax = Math.max(upMax, diff);
+                } else if (diff < 0) {  
+                    downMax = Math.max(downMax, -diff); 
+                }
+            }
+
+            System.out.println("#" + test_case + " " + upMax + " " + downMax);
+        }
+    }
+}
