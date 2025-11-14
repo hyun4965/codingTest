8bebeff3da477bf9d11b492d8e73560ab4f0e0a3
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-14 19:24:54 +0900

    [level 3] Title: 최고의 집합, Time: 1.75 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,25 +1,19 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int n, int s) {
-
-        if (s < n) {
-            return new int[]{-1};
-        }
-        
-        int[] answer = new int[n];
-
-        int base = s / n; 
-        int r = s % n;    
-        
-        for (int i = 0; i < n; i++) {
-            answer[i] = base;
-        }
-        
-        for (int i = n - r; i < n; i++) {
-            if (i >= 0) {
-                answer[i]++; 
-            }
-        }
-
-        return answer;
-    }
+  public int[] solution(int n, int s) {
+      if(n > s)
+          return new int[]{-1};
+      
+      int[] answer = new int[n];
+      for(int i=0; i<answer.length; i++){
+          answer[i] = s/n;
+      }
+      for(int i=0; i<s%n; i++){
+          answer[i] ++;
+      }
+      Arrays.sort(answer);
+      
+      return answer;
+  }
 }
