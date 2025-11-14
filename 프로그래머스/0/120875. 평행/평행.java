a7d4252e4880c85a7db54a22be3bc552ff6fcc24
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-14 21:30:55 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,17 +1,26 @@
 class Solution {
     public int solution(int[][] dots) {
-        if (parallel(dots[0], dots[1], dots[2], dots[3])) return 1;
-        if (parallel(dots[0], dots[2], dots[1], dots[3])) return 1;
-        if (parallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) {
+            return 1;
+        }
+
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) {
+            return 1;
+        }
+
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) {
+            return 1;
+        }
 
         return 0;
     }
 
-    private boolean parallel(int[] a, int[] b, int[] c, int[] d) {
-        long dy1 = (long) b[1] - a[1];
-        long dx1 = (long) b[0] - a[0];
-        long dy2 = (long) d[1] - c[1];
-        long dx2 = (long) d[0] - c[0];
-        return dy1 * dx2 == dy2 * dx1;
+    private boolean isParallel(int[] p1, int[] p2, int[] p3, int[] p4) {
+        int x1 = p1[0], y1 = p1[1];
+        int x2 = p2[0], y2 = p2[1];
+        int x3 = p3[0], y3 = p3[1];
+        int x4 = p4[0], y4 = p4[1];
+
+        return (y2 - y1) * (x4 - x3) == (y4 - y3) * (x2 - x1);
     }
 }
