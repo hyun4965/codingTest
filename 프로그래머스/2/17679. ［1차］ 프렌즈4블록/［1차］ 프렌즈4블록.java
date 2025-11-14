2fe93156006b16645a7c9cc84a03685ec06b62d6
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-14 19:02:26 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 18.79 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,16 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(int m, int n, String[] board) {
         int answer = 0;
-        
         char[][] table = new char[m][n];
-        
         for(int i=0;i<m;i++){
             table[i]=board[m-i-1].toCharArray();
         }
-        
+        //몇번 지워질지 모르니 무한반복
         while(true){
         boolean flag=true;
         boolean[][] check =new boolean[m][n];
+        //블록체크
         for(int i=0;i<m-1;i++){
             for(int j=0;j<n-1;j++){
                 if(table[i][j]=='-'){
@@ -28,9 +27,11 @@ class Solution {
             }
         }
             
+         //블록체크가 안됬으면 == 지울 블록이없으면 반복문 종료
         if(flag){
             break;
         }
+        //체크된 블록 삭제("-"으로 변환)
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(check[i][j]){
@@ -39,6 +40,7 @@ class Solution {
                 }
             }
         }
+        //블록 내리기 (윗블록과 스왑하기)
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(table[i][j]=='-'){
