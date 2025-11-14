4f18afd067f57ccb0a11dc03e6aecb175025d591
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-14 19:35:58 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 131.32 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,35 +1,27 @@
+import java.util.*;
+
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         
-        for (int i = 0; i < numbers.length; i++) {
-            long x = numbers[i];
-
-            if (x % 2 == 0) {
-                answer[i] = x + 1;
+        for (int i = 0; i<numbers.length; i++) {
+            long result = numbers[i];
+            String target = Long.toBinaryString(numbers[i]);
+            
+            if (result % 2 == 0) {
+                answer[i] = result + 1;
             } else {
-                String binaryString = Long.toBinaryString(x);
-                int lastZeroIndex = binaryString.lastIndexOf('0');
+                int idx = target.lastIndexOf("0");
                 
-                if (lastZeroIndex == -1) {
-                    answer[i] = x + (long)Math.pow(2, binaryString.length()) - (long)Math.pow(2, binaryString.length() - 1);
+                if (idx == -1) {
+                    String tmp = "10" + target.substring(1, target.length());
+                    answer[i] = Long.parseLong(tmp, 2);
                 } else {
-
-                    
-                    long temp = x;
-                    long lsb = 1; 
-                    
-                    while ((temp & 1) == 1) {
-                        temp >>= 1;
-                        lsb <<= 1;
-                    }
-                    
-                    lsb >>= 1;
-                    answer[i] = x + lsb;
+                    String tmp = target.substring(0, idx) + "10" + target.substring(idx + 2, target.length());
+                    answer[i] = Long.parseLong(tmp, 2);
                 }
             }
         }
-        
         return answer;
     }
 }
