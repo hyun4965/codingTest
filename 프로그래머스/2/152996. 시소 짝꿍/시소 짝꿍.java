1d0ca5b0b29013608782103d3e15a8280c77fef8
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-14 21:13:47 +0900

    [level 2] Title: 시소 짝꿍, Time: 143.41 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -3,32 +3,22 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-    
         Arrays.sort(weights);
-        Map<Double, Integer> map = new HashMap<>();
+        Map<Double,Integer> map = new HashMap<>();
         
-        for(int i : weights){
-            double a = i * 1.0;
-            double b = (i * 2.0) / 3.0;
-            double c = (i * 1.0) / 2.0;
-            double d = (i * 3.0) / 4.0;
+        for(double weight : weights) {
+            double type1 = weight * 1.0;
+            double type2 = weight * 0.5;
+            double type3 = weight * 2.0 / 3.0;
+            double type4 = weight * 0.75;
             
-            if(map.containsKey(a)) {
-                answer += map.get(a);
-            }
-            if(map.containsKey(b)) {
-                answer += map.get(b);
-            }
-            if(map.containsKey(c)) {
-                answer += map.get(c);
-            }
-            if(map.containsKey(d)) {
-                answer += map.get(d);
-            }
-            map.put((i * 1.0), map.getOrDefault((i * 1.0), 0) + 1);
+            if(map.containsKey(type1)) answer += map.get(type1);
+            if(map.containsKey(type2)) answer += map.get(type2);
+            if(map.containsKey(type3)) answer += map.get(type3);
+            if(map.containsKey(type4)) answer += map.get(type4);
+            
+            map.put(weight, map.getOrDefault(weight,0)+1);
         }
-        
-        
         return answer;
     }
 }
