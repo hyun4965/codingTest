c6d52910d09b2fc1988dfe7a04f848e69c210b70
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-16 12:11:58 +0900

    [level 2] Title: 배달, Time: 3.59 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,61 +1,67 @@
 import java.util.*;
 
 class Solution {
-    class Node  {
-        int e;  
-        int cost; 
+    static class Node implements Comparable<Node> {
+        int vertex, weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
         
-        Node(int e, int cost) {
-            this.e = e;
-            this.cost = cost;
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.weight, other.weight);
         }
     }
     
-    public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-        
-        List<List<Node>> graph = new ArrayList<>(N + 1);
-        for(int i=0; i<=N; i++){
+    public int solution(int N, int[][] road, int K) {        
+        // 인접 리스트
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
         
-        
-        for (int[] r : road) {
-            int a = r[0], b = r[1], c = r[2];
-            graph.get(a).add(new Node(b, c));
-            graph.get(b).add(new Node(a, c)); 
+        // 간선 정보 입력
+        for (int i = 0; i < road.length; i++) {
+            int u = road[i][0];
+            int v = road[i][1];
+            int w = road[i][2];
+            
+            // 양방향으로 간선 추가
+            graph.get(u).add(new Node(v, w));
+            graph.get(v).add(new Node(u, w));
         }
         
-        final int INF = 1_000_000_000;
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, INF);
+        // 최단 시간 배열
+        int[] dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
-
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(n -> n.cost));
-        pq.offer(new Node(1, 0));
-
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            int v = cur.e, d = cur.cost;
-
-            if (d > dist[v]) {
-                continue; 
-            }
-            for (Node nx : graph.get(v)) {
-                int nd = d + nx.cost;
-                if (nd < dist[nx.e]) {
-                    dist[nx.e] = nd;
-                    pq.offer(new Node(nx.e, nd));
+        
+        // 우선순위 큐
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(1, 0));
+        
+        while (!q.isEmpty()) {
+            Node current = q.remove();
+            
+            if (current.weight > dist[current.vertex]) continue;
+            
+            for (Node next : graph.get(current.vertex)) {
+                int cost = next.weight + dist[current.vertex];
+                
+                if (cost < dist[next.vertex]) {
+                    dist[next.vertex] = cost;
+                    q.add(new Node(next.vertex, cost));
                 }
             }
         }
 
+        // K 이하 시간 내 배달 가능한 마을 개수 리턴
+        int answer = 0;
         for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K) {
-                answer++;
-            }
+            if (dist[i] <= K) answer++;
         }
         return answer;
-
     }
 }
