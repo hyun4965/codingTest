b1bbb80d5466a814c7546abe0429e58d8b81a895
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-19 19:05:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            int num = sc.nextInt();
+            count = 0;
+            dfs(0, num);
+            System.out.println(count);
+        }
+    }
+    static void dfs(int currentSum, int target) {
+        if (currentSum > target) {
+            return;
+        }
+        if (currentSum == target) {
+            count++;
+            return;
+        }
+
+        dfs(currentSum + 1, target);
+        dfs(currentSum + 2, target);
+        dfs(currentSum + 3, target);
+    }
+}
