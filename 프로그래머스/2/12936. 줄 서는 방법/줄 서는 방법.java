5e605f5560a36e639c4f180f18862f90568719a6
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-19 19:56:54 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -2,24 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, long k) {
-        int[] answer = new int[n];
-        List<Integer> list = new ArrayList<>();
-
-        long f = 1;
+        List<Integer> numbers = new ArrayList<>();
+        long factorial = 1;
         
         for (int i = 1; i <= n; i++) {
-            list.add(i);
-            f *= i;
+            numbers.add(i);
+            factorial *= i;
         }
-
-        k--;
-        int index = 0;
-        while (index < n) {
-            f = f / (n - index);
-            answer[index++] = list.remove((int) (k / f));
-            k = k % f;
-        }
-
+        
+        int[] answer = new int[n];
+        dfs(n, k - 1, numbers, factorial, answer, 0);
+        
         return answer;
+    }
+    
+    // 재귀 함수
+    private void dfs(int n, long k, List<Integer> numbers, long factorial, int[] answer, int depth) {
+        if (n == 0) return;
+        
+        long unit = factorial / n;
+        
+        int index = (int) (k / unit);
+        
+        answer[depth] = numbers.get(index);
+        numbers.remove(index);
+        
+        dfs(n - 1, k % unit, numbers, unit, answer, depth + 1);
     }
 }
