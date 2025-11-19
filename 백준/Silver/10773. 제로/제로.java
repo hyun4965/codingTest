3b8d6fa5400f4b29ee460e3fb682f56a18f73a4e
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-19 18:09:23 +0900

    [Silver IV] Title: 제로, Time: 720 ms, Memory: 110076 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i=0; i<k; i++) {
+            int num = sc.nextInt();
+            if(!stack.isEmpty() && num == 0) {
+                stack.pop();
+            }else{
+                stack.push(num);
+            }
+        }
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            sum+=stack.pop();
+        }
+        System.out.print(sum);
+    }
+}
