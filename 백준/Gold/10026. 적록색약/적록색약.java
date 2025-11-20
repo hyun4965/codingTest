80d4e90ae1ef510c84eec36f1466ff570a09b82e
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-20 20:01:54 +0900

    [Gold V] Title: 적록색약, Time: 208 ms, Memory: 19744 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,82 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static char[][] map;
+    static boolean[][] visited;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        map = new char[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = sc.next();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        int normalCount = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    bfs(i, j, map[i][j]);
+                    normalCount++;
+                }
+            }
+        }
+
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+
+        int blindCount = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    bfs(i, j, map[i][j]);
+                    blindCount++;
+                }
+            }
+        }
+
+        System.out.println(normalCount + " " + blindCount);
+    }
+
+    static void bfs(int x, int y, char targetColor) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    if (!visited[nx][ny] && map[nx][ny] == targetColor) {
+                        visited[nx][ny] = true; 
+                        q.add(new int[] {nx, ny});
+                    }
+                }
+            }
+        }
+    }
+}
