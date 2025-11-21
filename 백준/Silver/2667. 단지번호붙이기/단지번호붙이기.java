090dba92904b4e2042b5af928708d7346d13bb70
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-21 20:51:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int count;
+    
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static ArrayList<Integer> result = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = sc.next();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    count = 0;
+                    dfs(i, j);
+                    result.add(count);
+                }
+            }
+        }
+
+        Collections.sort(result);
+        
+        System.out.println(result.size());
+        for (int c : result) {
+            System.out.println(c);
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+        count++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
