6bf4f8dd2a6fb4353a048215dea56185446273b0
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-24 12:17:22 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 15.34 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,36 +1,22 @@
 class Solution
-
 {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};    
-    
     public int solution(int [][]board)
     {
-        int answer = 0;
-        
-        int[][] newBoard = new int[board.length+1][board[0].length+1];
-        
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[i].length; j++){
-                newBoard[i+1][j+1] = board[i][j];
-            }
-        }
-        
+        int n = board.length;
+        int m = board[0].length;
+
+        int[][] dp = new int[n + 1][m + 1];
         int max = 0;
-        
-        for(int i=1; i<newBoard.length; i++){
-            for(int j=1; j<newBoard[i].length; j++){
-                if(newBoard[i][j] == 1){
-                    int left = newBoard[i-1][j];    
-                    int up = newBoard[i][j-1]; 
-                    int leftUp = newBoard[i-1][j-1];
-                    int min = Math.min(left, Math.min(up, leftUp)); 
-                    newBoard[i][j] = min+1;
-                    max = Math.max(max, min+1);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (board[i-1][j-1] == 1) {
+                    dp[i][j] = Math.min( Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    max = Math.max(max, dp[i][j]);
                 }
             }
         }
-        answer = max * max;
-        return answer;
+
+        return max * max; 
     }
 }
