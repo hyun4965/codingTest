818a8bd1330363eb4e5a8f48daebac7db2cc8206
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-25 12:22:37 +0900

    [level 2] Title: 하노이의 탑, Time: 5.38 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -2,31 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    List<int[]> moveList;
+    private List<int[]> sequence;
 
     public int[][] solution(int n) {
-        moveList = new ArrayList<>();
+        sequence = new ArrayList<>();
         
-        hanoi(n, 1, 2, 3);
+        hanoi(n, 1, 3, 2);
         
-        int[][] answer = new int[moveList.size()][2];
-        for (int i = 0; i < moveList.size(); i++) {
-            answer[i] = moveList.get(i);
+        int[][] answer = new int[sequence.size()][2];
+        for(int i = 0; i < sequence.size(); i++){
+            answer[i] = sequence.get(i);
         }
         
         return answer;
     }
 
-    private void hanoi(int n, int start, int via, int end) {
-        if (n == 1) {
-            moveList.add(new int[]{start, end});
+    private void hanoi(int n, int start, int end, int via){
+        if(n == 1){
+            sequence.add(new int[]{start, end});
             return;
         }
-        
-        hanoi(n - 1, start, end, via);
-        
-        moveList.add(new int[]{start, end});
-        
-        hanoi(n - 1, via, start, end);
+
+        hanoi(n - 1, start, via, end);
+
+        sequence.add(new int[]{start, end});
+
+        hanoi(n - 1, via, end, start);
     }
 }
