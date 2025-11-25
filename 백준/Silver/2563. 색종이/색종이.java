85ba98b546b9ee73a10621fb449c29a4e852624a
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-25 18:14:38 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean[][] visited = new boolean[100][100]; 
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+
+            for(int j = x; j < x + 10; j++) {
+                for(int k = y; k < y + 10; k++) {
+                    if (j < 100 && k < 100) {
+                        visited[j][k] = true;
+                    }
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(visited[i][j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
