e09ff38d14456eb1b0c17cde440cdc461cd679af
BAE <50188017+hyun4965@users.noreply.github.com>
2025-11-25 16:50:45 +0900

    [Gold IV] Title: 오큰수, Time: 1952 ms, Memory: 329888 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] seq = new int[n]; 
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>(); 
+        
+        for (int i = 0; i < n; i++) {
+            seq[i] = sc.nextInt();
+            ans[i] = -1; 
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
+                ans[stack.pop()] = seq[i];
+            }
+
+            stack.push(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int val : ans) {
+            sb.append(val).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
