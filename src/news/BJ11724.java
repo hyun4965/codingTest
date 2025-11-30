a93b6af096f35f92d64ac32e8be028a742941d59
배석현 <bjh4966@naver.com>
2025-11-30 13:51:03 +0900

    251130

---
+++ src/news/BJ11724.java
@@ -0,0 +1,49 @@
+package news;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BJ11724 {
+    static boolean[] visited;
+    static List<List<Integer>> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 정점의 개수
+        int m = sc.nextInt(); // 간선의 개수
+
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int index) {
+        visited[index] = true;
+
+        for (int i : list.get(index)) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
---
+++ src/news/BJ1541.java
@@ -0,0 +1,32 @@
+package news;
+
+import java.util.Scanner;
+
+public class BJ1541 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        String[] minusParts = str.split("-");
+
+        int result = 0;
+
+        result += sumOfPlusGroup(minusParts[0]);
+
+        for (int i = 1; i < minusParts.length; i++) {
+            result -= sumOfPlusGroup(minusParts[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    private static int sumOfPlusGroup(String s) {
+        String[] plusParts = s.split("\\+");
+        int sum = 0;
+        for (String numStr : plusParts) {
+            sum += Integer.parseInt(numStr);
+        }
+        return sum;
+    }
+}
