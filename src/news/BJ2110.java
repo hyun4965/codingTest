b83dcec360dae6f627aa8e478141873227dc86cf
배석현 <bjh4966@naver.com>
2025-12-20 17:13:57 +0900

    251220

---
+++ src/news/BJ2110.java
@@ -0,0 +1,51 @@
+package news;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ2110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int low = 1;
+        int high = arr[n - 1];
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int position = 0;
+            int count = 1;
+            for (int i = 1; i < n; i++) {
+                if (arr[i] - arr[position] >= mid) {
+                    position = i;
+                    count++;
+                }
+            }
+
+            if (count < c) {
+                high = mid - 1;
+                continue;
+            }
+
+            low = mid + 1;
+
+        }
+
+        System.out.println(low - 1);
+    }
+}
