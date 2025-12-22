1b3d85fe4d9ce11f85617e80825717f80d05691f
BAE <50188017+hyun4965@users.noreply.github.com>
2025-12-22 20:46:08 +0900

    [Silver II] Title: 최대 힙, Time: 848 ms, Memory: 112548 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); // 최대힙
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(queue.poll()).append('\n');
+                }
+            } else {
+                queue.add(num);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
