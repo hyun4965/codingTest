348fbd32835735918d9459a60f290c85f3086711
배석현 <bjh4966@naver.com>
2026-01-08 08:15:31 +0900

    260107

---
+++ src/news/BJ2751.java
@@ -0,0 +1,24 @@
+package news;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ2751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
