837ca630e61074909c567cd15e8c17cfbb34c76d
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-14 13:02:13 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,19 +1,10 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] arr = new int[num_list.length];
-        Deque<Integer> que = new ArrayDeque<>();
-
-        for (int i = 0; i < num_list.length; i++) {
-            que.add(num_list[i]);
+        int[] answer = new int[num_list.length];
+        
+        for(int i=0; i < num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = que.pollLast(); 
-        }
-
-        return arr;
+        return answer;
     }
 }
