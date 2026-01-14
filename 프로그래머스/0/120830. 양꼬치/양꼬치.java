d2ce8fce9cde2c6b9f3bf18c3be1e2f324ec4a5b
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-14 12:53:08 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,15 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        int count = n / 10;
-        answer = n *12000 + k * 2000 - count * 2000;
+        
+        int service = n/10;
+        
+        if(service < 0 ){
+            service = 0 ;
+        }
+        
+        answer = n * 12000 + (k-service) * 2000;
+        
         return answer;
     }
 }
