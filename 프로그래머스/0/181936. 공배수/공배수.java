64da601b79528681376aa57121e4b014cd5f8004
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-24 14:55:55 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        if((number % n == 0) && (number % m == 0)){
+        int answer = 0;
+        
+        if(number % n == 0  && number % m ==0){
             return 1;
         }else{
             return 0;
         }
-        
     }
 }
