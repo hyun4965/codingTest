19f8236296dc77e330a7db26580d6525fac9f695
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-26 20:17:12 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        if (my_string.endsWith(is_suffix)) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
