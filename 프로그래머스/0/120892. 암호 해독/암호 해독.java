629332a9080d2495cc749d60da9be11e5bced132
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-27 08:19:59 +0900

    [level 0] Title: 암호 해독, Time: 0.18 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,12 @@
 class Solution {
     public String solution(String cipher, int code) {
         StringBuilder answer = new StringBuilder();
-
-        for (int i = code - 1; i < cipher.length(); i += code) {
-            answer.append(cipher.charAt(i));
+        
+        for(int i=code-1; i<cipher.length(); i+=code){
+            char c = cipher.charAt(i);
+            answer.append(c);
         }
-
+        
         return answer.toString();
     }
 }
