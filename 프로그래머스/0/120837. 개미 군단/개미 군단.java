a4ca21a7874e3e267bcf963fadcbd0c7c017fe4e
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-26 21:54:38 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
+        int count = 0;
 
-        answer += hp / 5;   
-        hp %= 5;        
+        count += hp / 5;
+        hp %= 5;
 
+        count += hp / 3;
+        hp %= 3;
 
-        answer += hp / 3;   
-        hp %= 3;          
+        count += hp; 
 
-
-        answer += hp;      
-
-        return answer;
+        return count;
     }
 }
