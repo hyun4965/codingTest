bd313183edca5046864aacd6837e28b6953da4ac
BAE <50188017+hyun4965@users.noreply.github.com>
2026-01-28 08:09:37 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.10 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-
-        while (true) {
+        List<Integer> list = new  ArrayList<>();
+        
+        while(n>0){
             list.add(n);
-            if (n == 1) break;
-
-            if (n % 2 == 0) {
-                n = n / 2;
-            } else {
-                n = 3 * n + 1;
+            if(n==1){
+                break;
+            }
+            if(n % 2 == 0 ){
+                n/=2;
+            }else{
+                n=3*n + 1;
             }
         }
-
+        
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for(int i=0; i<list.size(); i++){
             answer[i] = list.get(i);
         }
-
+    
+        
         return answer;
     }
 }
