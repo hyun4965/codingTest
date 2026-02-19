303882b939d74798882dda74f31c1c34f75e75c1
BAE <50188017+hyun4965@users.noreply.github.com>
2026-02-20 08:11:11 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(String binomial) {
-        String[] parts = binomial.split(" ");
-        int a = Integer.parseInt(parts[0]);
-        String op = parts[1];
-        int b = Integer.parseInt(parts[2]);
-
-        switch (op) {
-            case "+":
-                return a + b;
-            case "-":
-                return a - b;
-            case "*":
-                return a * b;
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + op);
+        String[] str = binomial.split(" ");
+        
+        int a = Integer.parseInt(str[0]);
+        String op = str[1];
+        int b = Integer.parseInt(str[2]);
+        
+        if (op.equals("+")) {
+            return a + b;
+        } else if (op.equals("-")) {
+            return a - b;
+        } else { 
+            return a * b;
         }
+        
     }
 }
