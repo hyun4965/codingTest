4ce10a3cf8fa5e883a2c3193fd324e9cc88f772c
BAE <50188017+hyun4965@users.noreply.github.com>
2026-02-23 22:13:19 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.93 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,23 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int[] interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-            
-            for (int i = start; i <= end; i++) {
-                list.add(arr[i]);
-            }
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
+
+        int len1 = b1 - a1 + 1;
+        int len2 = b2 - a2 + 1;
+
+        int[] answer = new int[len1 + len2];
+        int idx = 0;
+
+        for (int i = a1; i <= b1; i++) {
+            answer[idx++] = arr[i];
         }
-        
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        for (int i = a2; i <= b2; i++) {
+            answer[idx++] = arr[i];
         }
-        
+
         return answer;
     }
 }
