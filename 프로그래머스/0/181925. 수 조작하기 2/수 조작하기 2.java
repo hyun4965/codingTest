38d486f44f76aacb42a2008e45680ab315b4b3a3
hyun <50188017+hyun4965@users.noreply.github.com>
2026-02-27 07:38:19 +0900

    [level 0] Title: 수 조작하기 2, Time: 6.12 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,21 +1,16 @@
 class Solution {
     public String solution(int[] numLog) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 1; i < numLog.length; i++) {
-            int n = numLog[i] - numLog[i - 1];
+            int diff = numLog[i] - numLog[i - 1];
 
-            if (n == 1) {
-                sb.append('w');
-            } else if (n == -1) {
-                sb.append('s');
-            } else if (n == 10) {
-                sb.append('d');
-            } else if (n == -10) {
-                sb.append('a');
-            }
+            if (diff == 1) answer.append('w');
+            else if (diff == -1) answer.append('s');
+            else if (diff == 10) answer.append('d');
+            else if (diff == -10) answer.append('a');
         }
 
-        return sb.toString();
+        return answer.toString();
     }
 }
