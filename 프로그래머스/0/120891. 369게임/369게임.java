346e13f2b13514a4aec97a6a8125f7f443a68e02
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-01 16:12:23 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        while (order > 0) {
-            int n = order % 10; 
-            if (n == 3 || n == 6 || n == 9) {
+        while(order>0){
+            int num = order%10;
+            if(num == 3 || num == 6 || num == 9){
                 answer++;
             }
-            order /= 10; 
+            order/=10;
         }
         return answer;
     }
