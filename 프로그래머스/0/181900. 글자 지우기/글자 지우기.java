034b9e37e23fbb3f42798e6996e816fe5cfa2fcb
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-03 11:36:08 +0900

    [level 0] Title: 글자 지우기, Time: 0.05 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,21 +1,19 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
-
-        Set<Integer> indexSet = new HashSet<>();
-        for (int idx : indices) {
-            indexSet.add(idx);
-        }
-
         StringBuilder answer = new StringBuilder();
+        
+        boolean[] remove = new boolean[my_string.length()];
+        
+        for (int idx : indices) {
+            remove[idx] = true;
+        }
+        
         for (int i = 0; i < my_string.length(); i++) {
-            if (!indexSet.contains(i)) {
+            if (!remove[i]) {
                 answer.append(my_string.charAt(i));
             }
         }
-
+        
         return answer.toString();
     }
 }
