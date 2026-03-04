2acfdd9716b12cff56175cddc9edba69a58555aa
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-04 11:24:06 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-
-        for (int num : num_list) {
-            while (num != 1) {
-                num /= 2;
-                answer++;
+        
+        for(int i=0; i<num_list.length; i++){
+            int num = num_list[i];
+            while(num>1){
+                if(num%2 ==0){
+                    answer++;
+                    num/=2;
+                }else{
+                    answer++;
+                    num--;
+                    num/=2;
+                }
             }
         }
-
+        
         return answer;
     }
 }
