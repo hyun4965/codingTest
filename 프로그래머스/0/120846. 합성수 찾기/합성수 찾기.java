2a5bf52d4d6da34ad141ab1d460016fd7f836ec5
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-04 10:30:59 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,21 +1,23 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        for (int i = 2; i <= n; i++) {
-            int count = 0;
-
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-            }
-
-            if (count >= 3) {
+        for (int i = 1; i <= n; i++) {
+            if (isComposite(i)) {
                 answer++;
             }
         }
-
         return answer;
+    }
+
+    static boolean isComposite(int x) {
+        if (x < 4) {
+            return false; 
+        }
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
