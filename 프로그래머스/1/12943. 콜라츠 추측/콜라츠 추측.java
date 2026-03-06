b96093da62999f3db880e4edbfaf96a4f6d4aa24
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-06 10:10:30 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,22 @@
 class Solution {
     public int solution(int num) {
-        long n = num;  
-        int count = 0;
-        
-        while (n != 1) {
-            if (count >= 500) {
+        int answer = 0;
+        long n = num;
+
+        while(n > 1){
+            if(n % 2 == 0){
+                n /= 2;
+            }else{
+                n = n * 3 + 1;
+            }
+
+            answer++;
+
+            if(answer >= 500){
                 return -1;
             }
-            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
-            count++;
         }
-        
-        return count;
+
+        return answer;
     }
 }
