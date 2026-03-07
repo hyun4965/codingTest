952aa4a8b8e1402a665117cd96b451f57b19013c
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-07 21:11:59 +0900

    [level 0] Title: k의 개수, Time: 21.03 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String kStr = String.valueOf(k);
         
-        for (int start = i; start <= j; start++) {
-            String numStr = String.valueOf(start);
-            for (int index = 0; index < numStr.length(); index++) {
-                if (numStr.charAt(index) == kStr.charAt(0)) {
+        for(int num = i; num <= j; num++){
+            String str = String.valueOf(num);
+            
+            for(char c : str.toCharArray()){
+                if(c == (char)(k + '0')){
                     answer++;
                 }
             }
