97b19baf95eae8fa45f69d06d41872b643fbebaa
hyun <50188017+hyun4965@users.noreply.github.com>
2026-03-12 09:08:17 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -2,10 +2,10 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
         
-        for (int num : array) {
-            String str = Integer.toString(num);
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '7') {
+        for(int i=0; i<array.length; i++){
+            String str = String.valueOf(array[i]);
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j) == '7'){
                     answer++;
                 }
             }
