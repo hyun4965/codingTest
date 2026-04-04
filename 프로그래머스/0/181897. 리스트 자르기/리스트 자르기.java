0b1f721c98f15b59600c1085c55e4e21b4a2fbdf
hyun <50188017+hyun4965@users.noreply.github.com>
2026-04-04 15:46:47 +0900

    [level 0] Title: 리스트 자르기, Time: 3.66 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> list = new ArrayList<>();
+        
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
-
-        int[] result;
-
-        switch (n) {
-            case 1:
-                result = Arrays.copyOfRange(num_list, 0, b + 1);
-                break;
-            case 2:
-                result = Arrays.copyOfRange(num_list, a, num_list.length);
-                break;
-            case 3:
-                result = Arrays.copyOfRange(num_list, a, b + 1);
-                break;
-            case 4:
-                List<Integer> list = new ArrayList<>();
-                for (int i = a; i <= b; i += c) {
-                    list.add(num_list[i]);
-                }
-                result = list.stream().mapToInt(i -> i).toArray();
-                break;
-            default:
-                result = new int[0]; 
+        
+        int start = 0;
+        int end = num_list.length - 1;
+        int step = 1;
+        
+        if (n == 1) {
+            start = 0;
+            end = b;
+        } else if (n == 2) {
+            start = a;
+            end = num_list.length - 1;
+        } else if (n == 3) {
+            start = a;
+            end = b;
+        } else if (n == 4) {
+            start = a;
+            end = b;
+            step = c;
         }
-
-        return result;
+        
+        for (int i = start; i <= end; i += step) {
+            list.add(num_list[i]);
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
