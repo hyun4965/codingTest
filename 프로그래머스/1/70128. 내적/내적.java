4343c3b46528c23e92b8ad17413841cc3e5ba732
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-12 20:15:36 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
         
-        for(int i=0; i<a.length;i++){
-            answer += (a[i] * b[i]);
+        for(int i=0; i<a.length; i++){
+            answer += a[i] * b[i];
         }
         
         return answer;
