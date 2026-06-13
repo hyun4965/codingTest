406d15754574c0727417f85639c724c0889c5b8a
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-13 17:16:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,13 +1,17 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        for(int i = left; i <= right; i++) {
-            if(Math.sqrt(i) == (int)Math.sqrt(i)) {
+
+        for (int i = left; i <= right; i++) {
+            int sqrt = (int) Math.sqrt(i);
+
+            if (sqrt * sqrt == i) {
                 answer -= i;
             } else {
                 answer += i;
             }
         }
+
         return answer;
     }
 }
