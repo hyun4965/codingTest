5d369e4d20903e61573f30700780702e2da2a30e
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-21 16:02:43 +0900

    [level 0] Title: 소인수분해, Time: 0.12 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
-import java.util.List;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 list.add(i);
+
                 while (n % i == 0) {
                     n /= i;
                 }
@@ -15,6 +15,7 @@ class Solution {
         }
 
         int[] answer = new int[list.size()];
+
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
