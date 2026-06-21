d1fc3c0ff1c94b52bbf35442d5c0bc0c0af919f1
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-21 14:39:56 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,13 +1,13 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < code.length(); i++) {
             if (i % q == r) {
-                sb.append(code.charAt(i));
+                answer.append(code.charAt(i));
             }
         }
 
-        return sb.toString();
+        return answer.toString();
     }
 }
