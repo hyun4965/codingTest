20d95d9e3f711a2bb32fc3841de0e59a475fc22d
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-21 14:29:10 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -5,10 +5,13 @@ class Solution {
         for (int i = 0; i < str_list.length; i++) {
             if (str_list[i].equals("l")) {
                 return Arrays.copyOfRange(str_list, 0, i);
-            } else if (str_list[i].equals("r")) {
+            }
+            
+            if (str_list[i].equals("r")) {
                 return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        return new String[0]; 
+        
+        return new String[0];
     }
 }
