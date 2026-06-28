4f2671c6f5b1bada57601b7a4b5a9be89f07fc7b
hyun <50188017+hyun4965@users.noreply.github.com>
2026-06-29 00:10:16 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.27 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,20 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr, int k) {
         int[] answer = new int[k];
-        Arrays.fill(answer, -1);
+        boolean[] visited = new boolean[100001];
 
-        Set<Integer> set = new HashSet<>();
-        int index = 0; 
+        for (int i = 0; i < k; i++) {
+            answer[i] = -1;
+        }
 
-        for (int num : arr) {
-            if (!set.contains(num)) { 
-                set.add(num);
-                answer[index++] = num;
-                if (index == k) {
-                    break; 
-                }
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (index == k) {
+                break;
+            }
+
+            if (!visited[arr[i]]) {
+                visited[arr[i]] = true;
+                answer[index] = arr[i];
+                index++;
             }
         }
 
